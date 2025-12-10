c22925b2b8b9525bfc8258149eb144d7e856a419
vcleto <vcleto@gmail.com>
2025-12-10 15:56:19 -0300

    Nova tentativa de fazer a altura do iframe ser ajustada dinamicamente para não gerar uma segunda barra de rolagem na página e sem gerar espaço em branco no final na mudança de páginas maiores para páginas menores.

--- Publicação 2023/responsive-height.js
+++ Publicação 2023/responsive-height.js
@@ -1,7 +1,22 @@
-function sendHeight() {
-    const height = document.body.scrollHeight;
-    parent.postMessage(height, '*');
+function sendHeightRepeatedly() {
+    let lastHeight = 0;
+    let attempts = 0;
+ 
+    const interval = setInterval(() => {
+        const newHeight = document.body.scrollHeight;
+
+        if (newHeight !== lastHeight) {
+            parent.postMessage(newHeight, "*");
+            lastHeight = newHeight;
+        }
+ 
+        attempts++;
+
+        if (attempts > 20) {
+            clearInterval(interval);
+        }
+    }, 150);
 }
  
-window.onload = sendHeight;
-window.onresize = sendHeight;
+window.onload = sendHeightRepeatedly;
+window.onresize = sendHeightRepeatedly;
